1326235ac9568888c14925a3aa4c4ed9efee1522
fatwaabdusyukur <waengyoo@gmail.com>
2023-07-29 13:04:00 +0700

    Create header and footer

---
+++ assets/js/interactive.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  document
+    .querySelector("header button")
+    .addEventListener("click", (button) => {
+      const nav = document.querySelector(".nav-link-container");
+      nav.classList.toggle("hidden");
+
+      document.querySelectorAll(".hamburger-menu").forEach((span, index) => {
+        if (index === 0) span.classList.toggle("flip-one");
+        else if (index === 1) span.classList.toggle("dissepear");
+        else span.classList.toggle("flip-two");
+      });
+    });
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,8 @@ module.exports = {
   theme: {
     extend: {},
   },
+  fontFamily: {
+    tektur: ["tektur"],
+  },
   plugins: [],
 };
