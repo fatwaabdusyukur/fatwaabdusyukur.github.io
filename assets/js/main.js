f8f4e7b19283f76e055c03eda65123587d753a92
fatwaabdusyukur <waengyoo@gmail.com>
2023-07-31 20:30:08 +0700

    Create modal to display the project video

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,89 +55,135 @@ function displayApp() {
 
 displayApp();
 
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector).children.length > 0) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector).children.length > 0) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 window.onload = function () {
-  if (appEl.children) {
-    const phrases = ["Web Development", "Android Development"];
-    let sleepTime = 100;
-    let activePhrase = 0;
+  waitForElm("#app")
+    .then((app) => {
+      const phrases = ["Web Development", "Android Development"];
+      let sleepTime = 100;
+      let activePhrase = 0;
 
-    const reactiveText = async () => {
-      const text = document.querySelector(".jumbotron p .reactive-text");
+      const reactiveText = async () => {
+        const text = app.querySelector(".jumbotron p .reactive-text");
 
-      while (true) {
-        let phrase = phrases[activePhrase];
+        while (true) {
+          let phrase = phrases[activePhrase];
 
-        for (let i = 0; i < phrase.length; i++) {
-          text.innerText = phrase.substring(0, i + 1);
-          await sleep(sleepTime);
+          for (let i = 0; i < phrase.length; i++) {
+            text.innerText = phrase.substring(0, i + 1);
+            await sleep(sleepTime);
+          }
+
+          await sleep(sleepTime * 10);
+
+          for (let i = phrase.length; i > 0; i--) {
+            text.innerText = phrase.substring(0, i - 1);
+            await sleep(sleepTime);
+          }
+
+          await sleep(sleepTime * 5);
+
+          activePhrase =
+            activePhrase === phrases.length - 1 ? 0 : activePhrase + 1;
         }
+      };
 
-        await sleep(sleepTime * 10);
+      reactiveText();
 
-        for (let i = phrase.length; i > 0; i--) {
-          text.innerText = phrase.substring(0, i - 1);
-          await sleep(sleepTime);
-        }
-
-        await sleep(sleepTime * 5);
-
-        activePhrase =
-          activePhrase === phrases.length - 1 ? 0 : activePhrase + 1;
-      }
-    };
-
-    reactiveText();
-
-    document
-      .querySelector("header button")
-      .addEventListener("click", (button) => {
+      app.querySelector("header button").addEventListener("click", (button) => {
         button.preventDefault();
-        const nav = document.querySelector(".nav-link-container");
+        const nav = app.querySelector(".nav-link-container");
         nav.classList.toggle("hidden");
 
-        document.querySelectorAll(".hamburger-menu").forEach((span, index) => {
+        app.querySelectorAll(".hamburger-menu").forEach((span, index) => {
           if (index === 0) span.classList.toggle("flip-one");
           else if (index === 1) span.classList.toggle("dissepear");
           else span.classList.toggle("flip-two");
         });
       });
 
-    document
-      .querySelectorAll(".project-card .project-body button")
-      .forEach((elemen) => {
-        elemen.addEventListener("click", (button) => {
-          const paragraf = button.target.parentElement.querySelector("p");
-          paragraf.classList.toggle("truncate");
+      app
+        .querySelectorAll(".project-card .project-body button")
+        .forEach((elemen) => {
+          elemen.addEventListener("click", (button) => {
+            const paragraf = button.target.parentElement.querySelector("p");
+            paragraf.classList.toggle("truncate");
 
-          const text = paragraf.classList.contains("truncate")
-            ? "More..."
-            : "Hide";
+            const text = paragraf.classList.contains("truncate")
+              ? "More..."
+              : "Hide";
 
-          button.target.innerText = text;
+            button.target.innerText = text;
+          });
         });
-      });
 
-    document
-      .querySelectorAll(".carousel .carousel-button")
-      .forEach((button, index) => {
-        const slides = button.parentElement.querySelector("ul");
-        slides.children[0].dataset.active = true;
+      app
+        .querySelectorAll(".carousel .carousel-button")
+        .forEach((button, index) => {
+          const slides = button.parentElement.querySelector("ul");
+          slides.children[0].dataset.active = true;
 
-        button.addEventListener("click", () => {
-          const offset = index === 1 ? 1 : -1;
+          button.addEventListener("click", () => {
+            const offset = index === 1 ? 1 : -1;
 
-          const activeSlide = slides.querySelector("[data-active]");
-          let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-          if (newIndex < 0) newIndex = slides.children.length - 1;
-          if (newIndex >= slides.children.length) newIndex = 0;
+            const activeSlide = slides.querySelector("[data-active]");
+            let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+            if (newIndex < 0) newIndex = slides.children.length - 1;
+            if (newIndex >= slides.children.length) newIndex = 0;
 
-          slides.children[newIndex].dataset.active = true;
-          delete activeSlide.dataset.active;
+            slides.children[newIndex].dataset.active = true;
+            delete activeSlide.dataset.active;
+          });
         });
+
+      const modal = app.querySelector(".modal-container");
+
+      app
+        .querySelectorAll(
+          ".project-container .project-card .project-body .carousel .carousel-item"
+        )
+        .forEach((card) => {
+          card.addEventListener("click", (item) => {
+            const input =
+              item.target.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(
+                "input"
+              );
+            const title = input.dataset.title;
+            const url = input.value;
+
+            modal.classList.replace("hide-modal", "show-modal");
+
+            modal.querySelector(".modal .modal-header h2").innerText = title;
+            modal.querySelector(".modal .modal-body iframe").src = url;
+          });
+        });
+
+      modal.querySelector(".modal button").addEventListener("click", () => {
+        modal.classList.replace("show-modal", "hide-modal");
       });
-  }
+    })
+    .catch((err) => console.error(err));
 };
